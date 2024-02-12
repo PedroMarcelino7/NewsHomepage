8912a7ff3170fa48691b0ec7b83dc96d1b3ca70b
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-02-11 21:30:07 -0300

    adding images + gh-pages

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.css'
+
+import Logo from '../../assets/images/logo.svg'
+
+export default function Navbar() {
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
