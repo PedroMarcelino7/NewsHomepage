38c69d6381643ab5313212252a2473b7dce56777
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-02-12 08:31:28 -0300

    main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import './App.css'
+import styles from './App.module.css'
 import Navbar from './components/Navbar/Navbar'
+
+import MainImageMobile from './assets/images/image-web-3-mobile.jpg'
+import MainImageDesktop from './assets/images/image-web-3-desktop.jpg'
 
 function App() {
   return (
-    <>
+    <div className={styles.container}>
       <Navbar />
-    </>
+
+      <main>
+        <div>
+          <img src={MainImageMobile} alt="" />
+        </div>
+
+        <div>
+          <h1 className={styles.headline}>The Bright Future of Web 3.0?</h1>
+        </div>
+
+        <div>
+          <p className={styles.paragraph}>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
+            But it's really fulfilling its promise?</p>
+
+          <button className={styles.button}>READ MORE</button>
+        </div>
+      </main>
+    </div>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,19 @@
 import styles from './Navbar.module.css'
 
 import Logo from '../../assets/images/logo.svg'
+import Menu from '../../assets/images/icon-menu.svg'
+import MenuClose from '../../assets/images/icon-menu-close.svg'
 
 export default function Navbar() {
     return (
-        <nav>
-            
+        <nav className={styles.navbar}>
+            <div>
+                <img src={Logo} alt="Logo" />
+            </div>
+
+            <div>
+                <img src={Menu} alt="Menu" />
+            </div>
         </nav>
     )
 }
