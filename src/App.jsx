3952a24d0f1cfd3b7931449ac5c31dcd562d1dec
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-02-12 08:57:12 -0300

    news section

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,48 @@ function App() {
       <Navbar />
 
       <main>
-        <div>
-          <img src={MainImageMobile} alt="" />
-        </div>
+        <section>
+          <div>
+            <img src={MainImageMobile} alt="" />
+          </div>
 
-        <div>
-          <h1 className={styles.headline}>The Bright Future of Web 3.0?</h1>
-        </div>
+          <div>
+            <h1 className={styles.headline}>The Bright Future of Web 3.0?</h1>
+          </div>
 
-        <div>
-          <p className={styles.paragraph}>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
-            But it's really fulfilling its promise?</p>
+          <div>
+            <p className={styles.paragraph}>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
+              But it's really fulfilling its promise?</p>
 
-          <button className={styles.button}>READ MORE</button>
-        </div>
+            <button className={styles.button}>READ MORE</button>
+          </div>
+        </section>
+
+        <section className={styles.news_container}>
+          <h2>New</h2>
+
+          <div className={styles.news}>
+            <h3>Hydrogen VS Electric Cars</h3>
+
+            <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+          </div>
+
+          <div className={styles.divisor}></div>
+
+          <div className={styles.news}>
+            <h3>The Downsides of AI Artistry</h3>
+
+            <p>What are the possible adverse effects of on-demand AI image generation?</p>
+          </div>
+
+          <div className={styles.divisor}></div>
+
+          <div className={styles.news}>
+            <h3>Is VC Funding Drying Up?</h3>
+
+            <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
+          </div>
+        </section>
       </main>
     </div>
   )
